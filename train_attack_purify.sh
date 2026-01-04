bd052599265d96da03f390a163956e36d593acb0
YiHangJie <172345295@qq.com>
2026-01-04 23:01:52 +0800

    sync .sh file

--- train_attack_purify.sh
+++ train_attack_purify.sh
@@ -8,45 +8,46 @@ conda init
 
 conda activate torch
 
-dataset="thubenchmark"
+dataset="${1:-thubenchmark}"
+gpu_id="${2:-0}"
 
 commands=(
     # train
-    "python -u train_AT.py --dataset ${dataset} --model eegnet --at_strategy madry"
-    "python -u train_AT.py --dataset ${dataset} --model eegnet --at_strategy fbf"
-    "python -u train_AT.py --dataset ${dataset} --model eegnet --at_strategy trades" 
-    "python -u train_AT.py --dataset ${dataset} --model eegnet --at_strategy clean"
+    "python -u train_AT.py --dataset ${dataset} --model eegnet --at_strategy madry --gpu_id ${gpu_id}"
+    "python -u train_AT.py --dataset ${dataset} --model eegnet --at_strategy fbf --gpu_id ${gpu_id}"
+    "python -u train_AT.py --dataset ${dataset} --model eegnet --at_strategy trades --gpu_id ${gpu_id}" 
+    "python -u train_AT.py --dataset ${dataset} --model eegnet --at_strategy clean --gpu_id ${gpu_id}"
 
     # attack
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy clean --fold 0 --attack autoattack --eps 0.1"
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy madry --fold 0 --attack autoattack --eps 0.1"
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy fbf --fold 0 --attack autoattack --eps 0.1"
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy trades --fold 0 --attack autoattack --eps 0.1"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy clean --fold 0 --attack autoattack --eps 0.1 --gpu_id ${gpu_id}"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy madry --fold 0 --attack autoattack --eps 0.1 --gpu_id ${gpu_id}"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy fbf --fold 0 --attack autoattack --eps 0.1 --gpu_id ${gpu_id}"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy trades --fold 0 --attack autoattack --eps 0.1 --gpu_id ${gpu_id}"
 
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy clean --fold 0 --attack fgsm --eps 0.1"
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy madry --fold 0 --attack fgsm --eps 0.1"
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy fbf --fold 0 --attack fgsm --eps 0.1"
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy trades --fold 0 --attack fgsm --eps 0.1"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy clean --fold 0 --attack fgsm --eps 0.1 --gpu_id ${gpu_id}"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy madry --fold 0 --attack fgsm --eps 0.1 --gpu_id ${gpu_id}"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy fbf --fold 0 --attack fgsm --eps 0.1 --gpu_id ${gpu_id}"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy trades --fold 0 --attack fgsm --eps 0.1 --gpu_id ${gpu_id}"
 
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy clean --fold 0 --attack pgd --eps 0.1"
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy madry --fold 0 --attack pgd --eps 0.1"
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy fbf --fold 0 --attack pgd --eps 0.1"
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy trades --fold 0 --attack pgd --eps 0.1"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy clean --fold 0 --attack pgd --eps 0.1 --gpu_id ${gpu_id}"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy madry --fold 0 --attack pgd --eps 0.1 --gpu_id ${gpu_id}"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy fbf --fold 0 --attack pgd --eps 0.1 --gpu_id ${gpu_id}"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy trades --fold 0 --attack pgd --eps 0.1 --gpu_id ${gpu_id}"
 
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy clean --fold 0 --attack cw --eps 0.1"
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy madry --fold 0 --attack cw --eps 0.1"
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy fbf --fold 0 --attack cw --eps 0.1"
-    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy trades --fold 0 --attack cw --eps 0.1"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy clean --fold 0 --attack cw --eps 0.1 --gpu_id ${gpu_id}"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy madry --fold 0 --attack cw --eps 0.1 --gpu_id ${gpu_id}"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy fbf --fold 0 --attack cw --eps 0.1 --gpu_id ${gpu_id}"
+    "python -u attack.py --dataset ${dataset} --model eegnet --at_strategy trades --fold 0 --attack cw --eps 0.1 --gpu_id ${gpu_id}"
 
     # purify
-    "python purify.py --config PTR3d_8_2048_rank5_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack"
-    "python purify.py --config PTR3d_8_2048_rank10_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack"
-    "python purify.py --config PTR3d_8_2048_rank15_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack"
-    "python purify.py --config PTR3d_8_2048_rank20_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack"
-    "python purify.py --config PTR3d_8_2048_rank25_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack"
-    "python purify.py --config PTR3d_8_2048_rank30_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack"
-    "python purify.py --config PTR3d_8_2048_rank35_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack"
-    "python purify.py --config PTR3d_8_2048_rank40_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack"
+    "python purify.py --config PTR3d_8_2048_rank5_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack --gpu_id ${gpu_id}"
+    "python purify.py --config PTR3d_8_2048_rank10_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack --gpu_id ${gpu_id}"
+    "python purify.py --config PTR3d_8_2048_rank15_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack --gpu_id ${gpu_id}"
+    "python purify.py --config PTR3d_8_2048_rank20_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack --gpu_id ${gpu_id}"
+    "python purify.py --config PTR3d_8_2048_rank25_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack --gpu_id ${gpu_id}"
+    "python purify.py --config PTR3d_8_2048_rank30_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack --gpu_id ${gpu_id}"
+    "python purify.py --config PTR3d_8_2048_rank35_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack --gpu_id ${gpu_id}"
+    "python purify.py --config PTR3d_8_2048_rank40_3d_interpolate.yaml --sample_num 512 --dataset ${dataset} --attack autoattack --gpu_id ${gpu_id}"
 )
 
 # Run each command in the array in the background, with at most max_jobs concurrent jobs
@@ -63,3 +64,7 @@ done
 # Wait for all remaining background jobs to finish
 wait
 
+# nohup bash train_attack_purify.sh thubenchmark 0 > train_attack_purify_thubenchmark_gpu0.log 2>&1 &
+# nohup bash train_attack_purify.sh bciciv2a 1 > train_attack_purify_bciciv2a_gpu1.log 2>&1 &
+# nohup bash train_attack_purify.sh m3cv 2 > train_attack_purify_m3cv_gpu2.log 2>&1 &
+# nohup bash train_attack_purify.sh seediv 3 > train_attack_purify_seediv_gpu3.log 2>&1 &
